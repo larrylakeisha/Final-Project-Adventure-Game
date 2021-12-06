fefc9d95bbf29830f2c4947c20aac39a3aa09125
larrylakeisha <91972138+larrylakeisha@users.noreply.github.com>
2021-12-06 15:11:50 -0600

    Add files via upload

---
+++ Base.py
@@ -0,0 +1,10 @@
+import time
+import globalvariables
+
+
+def base():
+    globalvariables.global_variables() # global variables
+    startgame.schoolbuilding() #starts game
+
+if __name__ == "__base__":
+    base()
